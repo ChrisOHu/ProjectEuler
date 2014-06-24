2291046bab9091ce4815ea439efadf06e51716ba
HuaiShun Hu <hshu@ambarella.com>
2014-06-24 15:00:53 +0800

    print the full path for e244

--- e244.py
+++ e244.py
@@ -1,37 +1,46 @@
+import re
+
 start = (0, sum([1 << x for x in [1, 4, 5, 8, 9, 12, 13]]))
 stop = (0, sum([1 << x for x in [2, 5, 7, 8, 10, 13, 15]]))
 def get_next((white, reds)):
-  all_nexts = []
-  is_red = lambda x: (reds & (1 << x)) > 0
-  new_whites = []
-  if white > 3: new_whites.append((68, white - 4))
-  if white < 12: new_whites.append((85, white + 4))
-  if white % 4 != 0: new_whites.append((82, white - 1))
-  if white % 4 != 3: new_whites.append((76, white + 1))
-  for dir, new_white in new_whites:
-    if is_red(white) and not is_red(new_white):
-      new_reds = reds - (1 << white)
-    elif is_red(new_white) and not is_red(white):
-      new_reds = reds + (1 << white)
-    else:
-      new_reds = reds
-    all_nexts.append((dir, (new_white, new_reds)))
-  return all_nexts
+	all_nexts = []
+	is_red = lambda x: (reds & (1 << x)) > 0
+	new_whites = []
+	if white > 3: new_whites.append((white - 4, 'U'))
+	if white < 12: new_whites.append((white + 4, 'D'))
+	if white % 4 != 0: new_whites.append((white - 1, 'L'))
+	if white % 4 != 3: new_whites.append((white + 1, 'R'))
+	for new_white, code in new_whites:
+		if is_red(white) and not is_red(new_white):
+			new_reds = reds - (1 << white)
+		elif is_red(new_white) and not is_red(white):
+			new_reds = reds + (1 << white)
+		else:
+			new_reds = reds
+		all_nexts.append(((new_white, new_reds), code))
+	return all_nexts
 
-reachable = set([start])    # All the reachable states for the BFS
-current = {start: set([0])} # The different codes used to reach each state
+reachable = set([start])        # All the reachable states for BFS
+current_seq = {start: []}	    # The move sequences to reach current state
 
-while stop not in current:
-  print len(reachable)
-  tmp = {}
-  to_add = set()
-  for config, codes in current.iteritems():
-    for direction, next in get_next(config):
-      if not next in reachable:
-        to_add.add(next)
-        if not next in tmp: tmp[next] = set()
-        for code in codes:
-          tmp[next].add((code * 243 + direction) % 100000007)
-  reachable |= to_add
-  current = tmp
-print sum(current[stop])
+while stop not in current_seq:
+	print len(reachable)
+	tmp_seq = {}
+	to_add = set()
+	for config, codes in current_seq.iteritems():
+		for next, codes in get_next(config):
+			if not next in reachable:
+				to_add.add(next)
+				if not next in tmp_seq:
+					tmp_seq[next] = []
+				for code in codes:
+					tmp_seq[next].append(current_seq[config])
+					tmp_seq[next].append(code)
+	reachable |= to_add
+	current_seq = tmp_seq
+
+final_seq = ""
+for s in re.findall('[a-zA-Z]', str(current_seq[stop])):
+	final_seq += s
+	final_seq += ' '
+print final_seq
